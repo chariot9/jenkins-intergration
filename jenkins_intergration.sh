d0336c8b8ed750908f4923916bde84a8e3484721
Trung Vu <trungvu@Trungs-MacBook-Pro.local>
2019-10-19 03:30:13 +0900

    Create shellscript

--- jenkins_intergration.sh
+++ jenkins_intergration.sh
@@ -4,23 +4,15 @@ export REPO_NAME='jenkins-intergration'
 export JOB_NAME='jenkins-intergration'
 
 if [[ $TEST_ERROR == 0 ]] ; then
-  curl "https://api.github.com/repos/octocat/$REPO_NAME/statuses/$(git rev-parse HEAD)?access_token=$GH_TOKEN" \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{
-      \"state\": \"success\",
-      \"context\": \"jenkins/$REPO_NAME\",
-      \"description\": \"Jenkins\",
-      \"target_url\": \"https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\"
-  }"
+  curl -XPOST \
+    -H "Authorization: token $GH_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d "{\"state\": \"success\", \"target_url\": \"https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\", \"description\": \"Jenkins CI.\", \"context\": \"Jenkins CI\"}" \
+    https://api.github.com/repos/chariot9/$REPO_NAME/statuses/$(git rev-parse HEAD)
 else
-  curl "https://api.github.com/repos/octocat/$REPO_NAME/statuses/$(git rev-parse HEAD)?access_token=$GH_TOKEN" \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{
-      \"state\": \"failure\",
-      \"context\": \"jenkins/$REPO_NAME\",
-      \"description\": \"Jenkins\",
-      \"target_url\": \"https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\"
-  }"
+  curl -XPOST \
+    -H "Authorization: token $GH_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d "{\"state\": \"failure\", \"target_url\": \"https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\", \"description\": \"Jenkins CI.\", \"context\": \"Jenkins CI\"}" \
+    https://api.github.com/repos/chariot9/$REPO_NAME/statuses/$(git rev-parse HEAD)
 fi
