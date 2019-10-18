bdba5d4a8d7da1e2a1c730a7183bd49b446bb5e9
Trung Vu <trungvu@Trungs-MacBook-Pro.local>
2019-10-19 02:13:43 +0900

    Create shellscript

--- jenkins_intergration.sh
+++ jenkins_intergration.sh
@@ -3,8 +3,7 @@
 export REPO_NAME='jenkins-intergration'
 export JOB_NAME='jenkins-intergration'
 
-curl "http://api.github.com/repos/octocat/$REPO_NAME/statuses/$(git rev-parse HEAD)
-      ?access_token=8f7789285354639fafc8a4af599255e5c10b9d53" \
+curl "http://api.github.com/repos/octocat/$REPO_NAME/statuses/$(git rev-parse HEAD)?access_token=8f7789285354639fafc8a4af599255e5c10b9d53" \
 -H "Content-Type: application/json" \
 -X POST \
 -d "{
