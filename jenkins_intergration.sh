2edb7f20633a53c63648af89f6bb9bc1e1bde1a4
Trung Vu <trungvu@Trungs-MacBook-Pro.local>
2019-10-19 03:32:37 +0900

    Create shellscript abc

--- jenkins_intergration.sh
+++ jenkins_intergration.sh
@@ -5,13 +5,13 @@ export JOB_NAME='jenkins-intergration'
 
 if [[ $TEST_ERROR == 0 ]] ; then
   curl -XPOST \
-    -H "Authorization: token $GH_TOKEN" \
+    -H "Authorization: token 8f7789285354639fafc8a4af599255e5c10b9d53" \
     -H "Content-Type: application/json" \
     -d "{\"state\": \"success\", \"target_url\": \"https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\", \"description\": \"Jenkins CI.\", \"context\": \"Jenkins CI\"}" \
     https://api.github.com/repos/chariot9/$REPO_NAME/statuses/$(git rev-parse HEAD)
 else
   curl -XPOST \
-    -H "Authorization: token $GH_TOKEN" \
+    -H "Authorization: token 8f7789285354639fafc8a4af599255e5c10b9d53" \
     -H "Content-Type: application/json" \
     -d "{\"state\": \"failure\", \"target_url\": \"https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\", \"description\": \"Jenkins CI.\", \"context\": \"Jenkins CI\"}" \
     https://api.github.com/repos/chariot9/$REPO_NAME/statuses/$(git rev-parse HEAD)
