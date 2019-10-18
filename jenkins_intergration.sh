a2077fbed6e355d5ee1d5370bfe8ef79679f9781
Trung Vu <trungvu@Trungs-MacBook-Pro.local>
2019-10-19 02:01:07 +0900

    Create shellscript

--- jenkins_intergration.sh
+++ jenkins_intergration.sh
@@ -3,7 +3,7 @@
 export REPO_NAME='jenkins-intergration'
 export JOB_NAME='jenkins-intergration'
 
-curl -X POST -H "Authorization: token 8f7789285354639fafc8a4af599255e5c10b9d53" https://api.github.com/repos/:owner/:repo/statuses/$(git rev-parse HEAD) -d "{ \
+curl -X POST -H "Authorization: token 8f7789285354639fafc8a4af599255e5c10b9d53" https://api.github.com/repos/octocat/$REPO_NAME/statuses/$(git rev-parse HEAD) -d "{ \
   \"state\": \"failure\", \
   \"target_url\": \"//https://81276af2.ngrok.io/job/$JOB_NAME/$BUILD_NUMBER/console\", \
   \"description\": \"The build has failed.\" \
