73f2d2c7efdfc6b37d629f851b599e1bbd2487d1
Trung Vu <trungvu@Trungs-MacBook-Pro.local>
2019-10-19 11:22:07 +0900

    Add formater

--- src/test/java/com/chariot9/jenkins/UserTest.java
+++ src/test/java/com/chariot9/jenkins/UserTest.java
@@ -7,36 +7,36 @@ import static org.junit.Assert.*;
 
 public class UserTest {
 
-    @Test
-    public void getName() {
-        User user = new User("Trung", 26);
-        assertThat(user.getName(), Is.is("Trung"));
-    }
+  @Test
+  public void getName() {
+    User user = new User("Trung", 26);
+    assertThat(user.getName(), Is.is("Trung"));
+  }
 
-    @Test
-    public void getAge() {
-        User user = new User("Trung", 26);
-        assertThat(user.getAge(), Is.is(26));
-    }
+  @Test
+  public void getAge() {
+    User user = new User("Trung", 26);
+    assertThat(user.getAge(), Is.is(26));
+  }
 
-    @Test
-    public void getAgeAndName() {
-        User user = new User("Trung", 26);
-        assertThat(user.getName(), Is.is("Trung"));
-        assertThat(user.getAge(), Is.is(26));
-    }
+  @Test
+  public void getAgeAndName() {
+    User user = new User("Trung", 26);
+    assertThat(user.getName(), Is.is("Trung"));
+    assertThat(user.getAge(), Is.is(26));
+  }
 
-    @Test
-    public void getAgeAndName2() {
-        User user = new User("Trung", 26);
-        assertThat(user.getName(), Is.is("Trung"));
-        assertThat(user.getAge(), Is.is(26));
-    }
+  @Test
+  public void getAgeAndName2() {
+    User user = new User("Trung", 26);
+    assertThat(user.getName(), Is.is("Trung"));
+    assertThat(user.getAge(), Is.is(26));
+  }
 
-    @Test
-    public void getAgeAndName3  () {
-        User user = new User("Trung", 26);
-        assertThat(user.getName(),   Is.is("Trung"));
-        assertThat(user.getAge(), Is.is(26));
-    }
+  @Test
+  public void getAgeAndName3() {
+    User user = new User("Trung", 26);
+    assertThat(user.getName(), Is.is("Trung"));
+    assertThat(user.getAge(), Is.is(26));
+  }
 }
